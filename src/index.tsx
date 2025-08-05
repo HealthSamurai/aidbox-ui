c15aeac1954f561c7ddfc9880dac3a577ef248f3
Vasilii Kupriakov <vasiliy.kupryakov@health-samurai.io>
2025-08-05 22:19:43 +0800

    Add TanStack Router

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,22 @@
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
+}
+
 const root = document.getElementById("root");
 if (root) {
-	createRoot(root).render(<StrictMode>Aidbox UI</StrictMode>);
+	createRoot(root).render(
+		<StrictMode>
+			<RouterProvider router={router} />
+		</StrictMode>,
+	);
 }
