0382af154cf56f0fdc356ca9ed4238cee228907b
Panthevm <panthevm@yandex.ru>
2025-09-03 13:15:15 +0300

    Use tablistdropdown

--- src/components/rest/active-tabs.tsx
+++ src/components/rest/active-tabs.tsx
@@ -1,11 +1,10 @@
 import {
 	Tabs,
 	TabsAddButton,
-	TabsList,
+	TabsBrowserList,
+	TabsListDropdown,
 	TabsTrigger,
 } from "@health-samurai/react-components";
-
-
 
 export type TabId = string;
 
@@ -44,7 +43,10 @@ export const DEFAULT_TAB: Tab = {
 	params: [{ id: "1", name: "", value: "", enabled: true }],
 };
 
-function addTab(tabs: Tab[], setTabs: (val: Tab[] | ((prev: Tab[]) => Tab[])) => void) {
+function addTab(
+	tabs: Tab[],
+	setTabs: (val: Tab[] | ((prev: Tab[]) => Tab[])) => void,
+) {
 	const newTab: Tab = {
 		...DEFAULT_TAB,
 		id: crypto.randomUUID(),
@@ -52,7 +54,11 @@ function addTab(tabs: Tab[], setTabs: (val: Tab[] | ((prev: Tab[]) => Tab[])) =>
 	setTabs([...tabs.map((t) => ({ ...t, selected: false })), newTab]);
 }
 
-function removeTab(tabs: Tab[], tabId: TabId, setTabs: (val: Tab[] | ((prev: Tab[]) => Tab[])) => void) {
+function removeTab(
+	tabs: Tab[],
+	tabId: TabId,
+	setTabs: (val: Tab[] | ((prev: Tab[]) => Tab[])) => void,
+) {
 	const newTabs = tabs.filter((tab) => tab.id !== tabId);
 	if (newTabs.length === 0) {
 		setTabs([DEFAULT_TAB]);
@@ -66,20 +72,24 @@ function removeTab(tabs: Tab[], tabId: TabId, setTabs: (val: Tab[] | ((prev: Tab
 			const targetIndex = removedTabIndex > 0 ? removedTabIndex - 1 : 0;
 			// Make sure we don't go out of bounds in the new array
 			const safeIndex = Math.min(targetIndex, newTabs.length - 1);
-			
+
 			updatedTabs = newTabs.map((tab, idx) =>
-				idx === safeIndex ? { ...tab, selected: true } : { ...tab, selected: false },
+				idx === safeIndex
+					? { ...tab, selected: true }
+					: { ...tab, selected: false },
 			);
 		}
 		setTabs(updatedTabs);
 	}
 }
 
-function onTabSelect(tabId: TabId, tabs: Tab[], setTabs: (val: Tab[] | ((prev: Tab[]) => Tab[])) => void) {
+function onTabSelect(
+	tabId: TabId,
+	tabs: Tab[],
+	setTabs: (val: Tab[] | ((prev: Tab[]) => Tab[])) => void,
+) {
 	setTabs(tabs.map((t) => ({ ...t, selected: t.id === tabId })));
 }
-
-
 
 const methodColors = {
 	GET: "text-utility-green",
@@ -88,6 +98,15 @@ const methodColors = {
 	PATCH: "text-utility-violet",
 	DELETE: "text-utility-red",
 };
+
+function TabContent({ tab }: { tab: Tab }) {
+	return (
+		<span className="flex items-center gap-1 truncate">
+			<span className={methodColors[tab.method]}>{tab.method}</span>
+			<span>{tab.path || tab.name}</span>
+		</span>
+	);
+}
 
 export function ActiveTabs({
 	tabs,
@@ -106,24 +125,31 @@ export function ActiveTabs({
 
 	return (
 		<Tabs variant="browser" value={selectedTab}>
-			<TabsList>
+			<TabsBrowserList>
 				{tabs.map((tab) => {
 					return (
 						<TabsTrigger
 							key={tab.id}
 							value={tab.id}
-							{...(tabs.length > 1 && { onClose: () => handleCloseTab(tab.id) })}
+							{...(tabs.length > 1 && {
+								onClose: () => handleCloseTab(tab.id),
+							})}
 							onClick={() => handleTabSelect(tab.id)}
 						>
-							<span className="flex items-center gap-1 truncate">
-								<span className={methodColors[tab.method]}>{tab.method}</span>
-								<span>{tab.path || tab.name}</span>
-							</span>
+							<TabContent tab={tab} />
 						</TabsTrigger>
 					);
 				})}
-			</TabsList>
+			</TabsBrowserList>
 			<TabsAddButton onClick={() => addTab(tabs, setTabs)} />
+			<TabsListDropdown
+				tabs={tabs.map((tab) => ({
+					id: tab.id,
+					content: <TabContent tab={tab} />,
+				}))}
+				handleTabSelect={handleTabSelect}
+				handleCloseTab={handleCloseTab}
+			/>
 		</Tabs>
 	);
 }
