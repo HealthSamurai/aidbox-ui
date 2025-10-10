9127c066c2b33eac0863d20d7477022306143c05
Panthevm <panthevm@yandex.ru>
2025-10-10 18:51:03 +0300

    [HealthSamurai/sansara#6378] Remove deprecated resource list page

--- src/routes/resource-types.$resourceType.index.tsx
+++ src/routes/resource-types.$resourceType.index.tsx
@@ -1,5 +1,4 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { Resources } from "../components/ResourceBrowser/resources";
 import { ResourcesPage } from "../components/ResourceBrowser/resources-page";
 
 export const Route = createFileRoute("/resource-types/$resourceType/")({
