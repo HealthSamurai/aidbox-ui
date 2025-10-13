414b1985bc6308e1fd982265ef98d5a01fe2a027
Aleksandr Penskoi <alexandr.penskoi@health-samurai.io>
2025-10-13 15:41:18 +0200

    Move current tab from localStore to search path and types.

--- src/routes/resource-types.$resourceType.$id.tsx
+++ src/routes/resource-types.$resourceType.$id.tsx
@@ -1,3 +1,4 @@
+import type { ViewDefinitionEditorTab } from "@aidbox-ui/components/ViewDefinition/types";
 import { createFileRoute } from "@tanstack/react-router";
 import ViewDefinitionPage from "../components/ViewDefinition/page";
 
@@ -11,8 +12,26 @@ const PageComponent = () => {
 	}
 };
 
+type ResourcePageSearch = {
+	tab: ViewDefinitionEditorTab;
+};
+
+const tabs: Set<ViewDefinitionEditorTab> = new Set(["form", "code", "sql"]);
+
 export const Route = createFileRoute("/resource-types/$resourceType/$id")({
 	component: PageComponent,
+	validateSearch: (search: Record<string, unknown>): ResourcePageSearch => {
+		let tab: ViewDefinitionEditorTab;
+		if (tabs.has(search.tab as ViewDefinitionEditorTab)) {
+			tab = search.tab as ViewDefinitionEditorTab;
+		} else if (search.tab === undefined) {
+			tab = "code";
+		} else {
+			console.error("Invalid tab", search.tab, "force to 'code'");
+			tab = "code";
+		}
+		return { tab };
+	},
 	staticData: {
 		title: "View Definition",
 	},
