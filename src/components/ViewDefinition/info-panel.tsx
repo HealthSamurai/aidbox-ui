84836e2101cc756dbd62e63caaf51ea7ae37f6ad
Aleksandr Penskoi <alexandr.penskoi@health-samurai.io>
2025-10-14 12:57:54 +0200

    Tiding.

--- src/components/ViewDefinition/info-panel.tsx
+++ src/components/ViewDefinition/info-panel.tsx
@@ -3,18 +3,17 @@ import { useLocalStorage } from "../../hooks/useLocalStorage";
 import { ExampleTabContent } from "./example-tab-content";
 import { SchemaTabContent } from "./schema-tab-content";
 
+export type InfoPanelTabs = "schema" | "examples";
+
 export function InfoPanel() {
-	const [activeTab, setActiveTab] = useLocalStorage<"schema" | "examples">({
+	const [activeTab, setActiveTab] = useLocalStorage<InfoPanelTabs>({
 		key: `viewDefinition-infoPanel-activeTab`,
 		defaultValue: "schema",
 	});
 
 	return (
 		<div className="flex flex-col h-full">
-			<Tabs
-				value={activeTab}
-				onValueChange={(value) => setActiveTab(value as "schema" | "examples")}
-			>
+			<Tabs value={activeTab} onValueChange={(value) => setActiveTab(value)}>
 				<div className="flex items-center justify-between bg-bg-secondary pl-6 pr-2 py-3 border-b h-10">
 					<div className="flex items-center gap-8">
 						<TabsList>
