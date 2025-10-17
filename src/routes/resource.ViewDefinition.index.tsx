f10b5959efcb973ace6c62c35428251466dd72d3
Aleksandr Penskoi <alexandr.penskoi@health-samurai.io>
2025-10-17 14:15:54 +0200

    Fix /resource/ViewDefinition breadcrumber.

---
+++ src/routes/resource.ViewDefinition.index.tsx
@@ -0,0 +1,13 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { ResourcesPage } from "../components/ResourceBrowser/resources-page";
+
+export const Route = createFileRoute("/resource/ViewDefinition/")({
+	component: RouteComponent,
+	staticData: {
+		title: "View Definition",
+	},
+});
+
+function RouteComponent() {
+	return <ResourcesPage resourceType={"ViewDefinition"} />;
+}
